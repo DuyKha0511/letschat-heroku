7bbe64666aa28f42897e3d805febd980b56a5c66
DuyKha0511 <67335618+DuyKha0511@users.noreply.github.com>
2020-12-21 22:29:01 +0700

    Update server.js

--- server.js
+++ server.js
@@ -14,9 +14,9 @@ var dateformat = require('dateformat');
 app.use(bodyParser.json({limit: "50mb"}));
 app.use(bodyParser.urlencoded({limit: "50mb", extended: true, parameterLimit:50000}));
 
-app.use(express.static(path.join(__dirname, '../clients/build')));
+app.use(express.static(path.join(__dirname, 'build')));
 app.get('/', (req, res) => {
-  res.sendFile(path.join(__dirname, '../clients/build/index.html'))
+  res.sendFile(path.join(__dirname, 'build/index.html'))
 })
 
 var conn=require('./Controller/DB').con;
