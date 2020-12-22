38d6f2a661cc493678a95facf03ce0d25ee5973a
DuyKha0511 <67335618+DuyKha0511@users.noreply.github.com>
2020-12-22 15:19:36 +0700

    Update server.js

--- server.js
+++ server.js
@@ -15,8 +15,8 @@ app.use(bodyParser.json({limit: "50mb"}));
 app.use(bodyParser.urlencoded({limit: "50mb", extended: true, parameterLimit:50000}));
 
 app.use(express.static(path.join(__dirname, 'build')));
-app.get('letschat-heroku.herokuapp.com', (req, res) => {
-  res.sendFile(path.join(__dirname, 'build/index.html'))
+app.get('letschat-heroku.herokuapp.com/', (req, res) => {
+  res.sendFile(path.join(__dirname, './build/index.html'))
 })
 
 var conn=require('./Controller/DB').con;
