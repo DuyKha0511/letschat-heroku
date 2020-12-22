9e1a811461b91e3ad31f089aeda098b12d6a6071
DuyKha0511 <67335618+DuyKha0511@users.noreply.github.com>
2020-12-22 15:41:27 +0700

    Update server.js

--- server.js
+++ server.js
@@ -9,14 +9,15 @@ var io = require('socket.io')(http, {
     origin: '*',
   }
 });
+app.set("port", port);
 var dateformat = require('dateformat');
 
 app.use(bodyParser.json({limit: "50mb"}));
 app.use(bodyParser.urlencoded({limit: "50mb", extended: true, parameterLimit:50000}));
 
 app.use(express.static(path.join(__dirname, '/build')));
-app.get('*', (req, res) => {
-  res.redirect('/');
+app.get('/', (req, res) => {
+  //res.redirect('/');
   res.sendFile(path.join(__dirname, '/build/index.html'))
 })
 
